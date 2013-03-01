c85bddd87c33594befd032a4a23f87a22f3d9246
Nathaniel Flath <nflath@Nathaniels-MacBook-Air.local>
2013-03-01 10:48:01 -0800

    Updated licensing

--- cppdepends.py
+++ cppdepends.py
@@ -1,7 +1,26 @@
+# <one line to give the program's name and a brief idea of what it does.>
+#     Copyright (C) 2009-2013 Nathaniel Flath
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Contact Nathaniel Flath at: flat0103@gmail.com
+
 import re, os, sys, tempfile, collections, copy
 
 usage = """
-cppdepends v1.0.  (C) 2009, 2010 Nathaniel Flath.
+cppdepends v1.0.  (C) 2009-2013 Nathaniel Flath.
+flat0103@gmail.com
 http://github.com/nflath/cppdepends
 
 usage: python cppdepends.py dir out [threshold]
